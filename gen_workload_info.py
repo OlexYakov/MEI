dc5f8c8d069fc8bf7a969f0ebc7e206f04f60e6c
OlexYakov <olexyakov@DeusExMachina.localdomain>
2020-10-28 17:46:26 +0000

    changes to gen workload (now with -full flag) and data

--- gen_workload_info.py
+++ gen_workload_info.py
@@ -12,37 +12,46 @@ def ensure_created(dir):
             exit()
 
 
+def gen_info(wk_name):
+    wk_in_dir = in_dir+"/"+wk_name
+    if not os.path.exists(wk_in_dir):
+        print(f"Workload {wk_name} not found. Aborting.")
+        exit()
+
+    wk_out_dir = out_dir+"/"+wk_name
+    ensure_created(wk_out_dir)
+
+    wk_file_names = os.listdir(wk_in_dir)
+
+    for wk_file_name in wk_file_names:
+        inf_dir = wk_in_dir+"/"+wk_file_name
+        otf_dir = wk_out_dir+"/"+wk_file_name
+        with open(inf_dir, "r") as inf, open(otf_dir, "w") as outf:
+            outf.write("nbursts cpu_time io_time")
+            while True:  # simultaneous read and write, dont need to keep all input in memory
+                line = inf.readline()
+                if not line:
+                    break
+                if "#" in line:
+                    continue
+                times = line.split()
+                nbursts = len(times)//2 - 1
+                cpu_time = sum([float(i) for i in times[0:-1:2]])
+                io_time = sum([float(i) for i in times[1:-1:2]])
+                outf.write(f"\n{nbursts} {cpu_time} {io_time}")
+
+
 out_dir = "./outputs_gen_info"
 ensure_created(out_dir)
 in_dir = "./outputs_gen"
 
-if (len(sys.argv) != 2):
-    print("Usage: gen_workload_info.py workloadname")
-    exit()
-
-wk_name = sys.argv[1]
-wk_in_dir = in_dir+"/"+wk_name
-if not os.path.exists(wk_in_dir):
-    print(f"Workload {wk_name} not found. Aborting.")
-    exit()
-
-wk_out_dir = out_dir+"/"+wk_name
-ensure_created(wk_out_dir)
-
-wk_file_names = os.listdir(wk_in_dir)
-
-for wk_file_name in wk_file_names:
-    inf_dir = wk_in_dir+"/"+wk_file_name
-    otf_dir = wk_out_dir+"/"+wk_file_name
-    with open(inf_dir, "r") as inf, open(otf_dir, "w") as outf:
-        while True:  # simultaneous read and write, dont need to keep all input in memory
-            line = inf.readline()
-            if not line:
-                break
-            if "#" in line:
-                continue
-            times = line.split()
-            nbursts = len(times)//2 - 1
-            cpu_time = sum([float(i) for i in times[0:-1:2]])
-            io_time = sum([float(i) for i in times[1:-1:2]])
-            outf.write(f"{nbursts} {cpu_time} {io_time}\n")
+if "-full" in sys.argv:
+    wks = os.listdir(in_dir)
+    for wk_name in wks:
+        gen_info(wk_name)
+else:
+    if (len(sys.argv) != 2):
+        print("Usage: gen_workload_info.py workloadname or -full")
+        exit()
+    wk_name = sys.argv[1]
+    gen_info(wk_name)
