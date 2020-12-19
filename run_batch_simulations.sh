533d710b880d3594499ed0f1558878b986f86e1f
olexyakov <olexyakov97@gmail.com>
2020-12-19 22:34:20 +0000

    new batch generator

--- run_batch_simulations.sh
+++ run_batch_simulations.sh
@@ -4,48 +4,111 @@ out_dir_simulator="outputs_sim"
 
 generator="python3 gen_workload.py"
 simulator="python3 simulator.py"
-#Round Robin Quantum Defautlt
+
+#defaults
+num_procs=20
+mean_io_bursts=10
+mean_iat=10
+min_CPU=1
+max_CPU=2
+min_IO=1
+max_IO=2
 quantum=10
 seeds=1
-if [[ $1 == "-h" ]];
-then
-    echo "./run_batch_simulations.sh [name] [nprocs] [mean_io_bursts] [mean_iat] [min_cpu] [max_cpu] [min_io] [max_io] [nseeds] [quant]"
-    exit
-fi
 
-if [ $# -lt 8 ]
-then
-    echo "Wrong amount of parameter passed"
-else
+
+usage() { 
+    echo "Usage: [name] [nproc] [io_bursts] [iat] [min_cpu] [max_cpu] [min_io] [max_io] [nseeds] [quant]" 1>&2; 
+    echo "Usage: -g ^" 1>&2; 
+    echo "Usage: -s NAME [SEEDS] [QUANT]" 1>&2; 
+    
+    exit 1;
+    }
+
+while getopts "hg:s:n:q:" o; do
+    case "${o}" in
+        g)
+            g=1
+            workload=${OPTARG}
+            ;;
+        s)
+            s=1
+            workload=${OPTARG}
+            ;;
+        n)
+            seeds=${OPTARG}
+            ;;
+        q)
+            quantum=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+shift $((OPTIND-2))
+echo "num " $#
+start=`date +%s`
+if [[ $g ]]; then 
+    # so gerar
+    if [[ $# -lt 8 ]]; then usage; fi
+    workload="$1"
+    workload_dir="$out_dir_generator/$workload"
+    mkdir -p $workload_dir
+    # Check optional arguments
     if [[ $# -ge 9 ]]; then seeds=$9; fi
     if [[ $# -ge 10 ]]; then quantum=${10}; fi
 
-    start=`date +%s`
-    # workload="Procs$1IOBurst$2IAT$3MinCPU$4MaxCPU$5MinIO$6MaxIO$7"
+    for ((i=1;i<=$seeds;i++)); do
+        input_file="$workload_dir/$i.csv"
+        echo "Generating workloads seed $i"
+        $generator $2 $3 $4 $5 $6 $7 $8 $i > $input_file 
+    done
+elif [[ $s ]]; then
+    # so simular
     workload="$1"
     workload_dir="$out_dir_generator/$workload"
     mkdir -p $workload_dir
     simulation_dir="$out_dir_simulator/$workload"
     mkdir -p $simulation_dir
+    rm $simulation_dir/*
+    
+    if [[ $# -ge 2 ]]; then seeds=$2; fi
+    if [[ $# -ge 3 ]]; then quantum=$3; fi
 
+    for ((i=1;i<=$seeds;i++)); do
+        input_file="$workload_dir/$i.csv"
+        ./run_simulator.sh $input_file $simulation_dir $quantum $i
+    done
+else
+    # Get arguments
+    if [[ $# -lt 8 ]]; then usage; fi
+    workload="$1"
+    workload_dir="$out_dir_generator/$workload"
+    mkdir -p $workload_dir
+    simulation_dir="$out_dir_simulator/$workload"
+    mkdir -p $simulation_dir
+    rm $simulation_dir/*
+
+    # Check optional arguments
+    if [[ $# -ge 9 ]]; then seeds=$9; fi
+    if [[ $# -ge 10 ]]; then quantum=${10}; fi
+    # gerar e simular
     for ((i=1;i<=$seeds;i++)); do
         input_file="$workload_dir/$i.csv"
 
         echo -en "\rGenerating workloads seed $i"
         $generator $2 $3 $4 $5 $6 $7 $8 $i > $input_file 
 
-        echo -en "\rSimulating workloads seed $i FCFS"
-        $simulator --cpu-scheduler fcfs < $input_file > "${simulation_dir}/FCFS_$i.txt"
-        echo -en "\rSimulating workloads seed $i RR  "
-        $simulator --cpu-scheduler rr --quantum $quantum  < $input_file > "${simulation_dir}/RR_$i.txt"
-        echo -en "\rSimulating workloads seed $i SJF"
-        $simulator --cpu-scheduler sjf < $input_file > "${simulation_dir}/SJF_$i.txt"
-        echo -en "\rSimulating workloads seed $i SRTF"
-        $simulator --cpu-scheduler srtf < $input_file > "${simulation_dir}/SRTF_$i.txt"
-        echo -e "\rSimulations for seed $i done.     "
+        ./run_simulator.sh $input_file $simulation_dir $quantum $i
     done
-
-    end=`date +%s`
-    runtime=$((end-start))
-    echo "Generating $1 took $runtime seconds"
 fi
+end=`date +%s`
+runtime=$((end-start))
+echo "Took $runtime seconds"
+
+
+
+
+
--- run_simulator.sh
+++ run_simulator.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-simulator="python3 ./simulator.py"
-input_file="./outputs_gen/inverse/1.csv"
-out_file="./outputs_sim/inverse"
-mkdir -p $out_file
-echo "Running only simulation for $input_file"
+simulator="python3 simulator.py"
 
-$simulator --cpu-scheduler fcfs < $input_file > "${out_file}/FCFS_1.txt"
-echo -en "\rSimulating workloads seed $i RR  "
-$simulator --cpu-scheduler rr --quantum 0.1  < $input_file > "${out_file}/RR_1.txt"
-echo -en "\rSimulating workloads seed $i SJF"
-$simulator --cpu-scheduler sjf < $input_file > "${out_file}/SJF_1.txt"
-echo -en "\rSimulating workloads seed $i SRTF"
-$simulator --cpu-scheduler srtf < $input_file > "${out_file}/SRTF_1.txt"
-echo -e "\rSimulations for seed $i done.     "
+if [[ $# -ne 4 ]] ; then
+    echo 
+    echo "Wrong number of arguments: [in] [out] [quant] [seed]";
+    echo $@
+    exit 1
+fi
+
+input_file=$1
+simulation_dir=$2
+quantum=$3
+seed=$4
+
+echo -en "\rSimulating workloads seed $seed FCFS"
+$simulator --cpu-scheduler fcfs < $input_file > "${simulation_dir}/FCFS_$seed.txt"
+echo -en "\rSimulating workloads seed $seed RR  "
+$simulator --cpu-scheduler rr --quantum $quantum  < $input_file > "${simulation_dir}/RR_$seed.txt"
+echo -en "\rSimulating workloads seed $seed SJF"
+$simulator --cpu-scheduler sjf < $input_file > "${simulation_dir}/SJF_$seed.txt"
+echo -en "\rSimulating workloads seed $seed SRTF"
+$simulator --cpu-scheduler srtf < $input_file > "${simulation_dir}/SRTF_$seed.txt"
+echo -e "\rSimulations for seed $seed done.     "
