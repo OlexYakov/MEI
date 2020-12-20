00ebe3f4e338394bc0a65bc7c47d23bdc652bdc6
olexyakov <olexyakov97@gmail.com>
2020-12-20 18:28:25 +0000

    hitpotesis changes

--- h2.sh
+++ h2.sh
@@ -2,11 +2,14 @@
 
 echo "Generating second hypotesis"
 
-seeds=120
+seeds=240
+first="long_solo_$seeds"
+second="short_bursts_$seeds"
+merged="mixed_h2_$seeds"
 
-./run_batch_simulations.sh -g long_solo 5 5 10 10 20 10 20 $seeds
-./run_batch_simulations.sh -g short_bursts 100 100 10 0.1 1 1 2 $seeds 
-./workload_merge.py short_bursts long_solo mixed_h2 
-./run_batch_simulations.sh -s mixed_h2 $seeds 0.1   
+./run_batch_simulations.sh -g $first 5 3 10 20 20 10 20 $seeds
+./run_batch_simulations.sh -g $second 100 100 10 0.5 0.5 1 2 $seeds 
+./workload_merge.py $first $second $merged 
+./run_batch_simulations.sh -s $merged $seeds 0.1   
 ./gen_workload_info.py -full  
 echo "Done"
